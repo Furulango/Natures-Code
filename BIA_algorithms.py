214eac699c1cf059d51a0600141660fefa28f8cc
Furulango <40371957+Furulango@users.noreply.github.com>
2025-10-27 19:11:16 -0600

    Add files via upload

---
+++ BIA_algorithms.py
@@ -0,0 +1,52 @@
+import torch as th
+
+# Part 1
+def genetic_algorithm(): # GA
+    pass
+
+def particle_swarm_optimization(): # PSO
+    pass
+
+def differential_evolution(): # DE
+    pass
+
+def cuckoo_search(): # CS
+    pass
+
+# Part 1.1 - Improved Variants
+def improved_particle_swarm_optimization(): # IPSO
+    pass
+
+def two_phase_particle_swarm_optimization(): # TPPSO
+    pass
+
+# Part 2 - Hybrid Algorithms
+
+def hybrid_genetic_algorithm_pso(): # GA-PSO
+    pass
+
+def gray_wolf_cuckoo_search(): # GWO-CS
+    pass
+
+def adaptative_weighted_gray_wolf_optimizer(): # AW-GWO
+    pass
+
+def newton_raphson_genetic_algorithm(): # NR-GA
+    pass
+
+# Part 3 - Other Bioinspired Algorithms
+def gray_wolf_optimizer(): # GWO    
+    pass
+
+def cat_swarm_optimization(): # CS
+    pass
+
+def bacterial_foraging_optimization(): # BFO
+    pass
+
+def artificial_bee_colony(): # ABC
+    pass
+
+
+
+
